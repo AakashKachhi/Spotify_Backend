520485f2fcb132a8fdd91ce0d94e1521866b9b3b
AakashKachhi <aakashkachhi67@gmail.com>
2026-08-06 13:09:06 +0500

    cr: some changes

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -26,4 +26,28 @@ async function authArtist(req, res, next) {
     }
 }
 
-export default {authArtist}
+async function authUser(req, res, next) {
+    const token = req.cookies.token
+
+    if(!token) {
+        return res.status(401).json({message: "Unauthorized"})
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        if(decoded.role !== "user" && decoded.role !== "artist") {
+            return res.status(403).json({message: "You don't have access"})
+        }
+
+        req.user = decoded
+
+        next()
+    }
+    catch (err) {
+        console.log(err)
+        return res.status(401).json({message: "Unauthorized"})
+    }
+}
+
+export default {authArtist, authUser}
